e549e15d7b1783601974e43714db7515779e7570
yoon <kimyam1008@gmail.com>
2025-12-29 09:59:55 +0900

    [신규] ThockPick 생성

---
+++ src/main/java/com/example/thockpickserver/ThockPickServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.thockpickserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThockPickServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ThockPickServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/thockpickserver/ThockPickServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.thockpickserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThockPickServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
